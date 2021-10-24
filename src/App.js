978715dc75aa7d6b42388d907b5636ec4f3710c1
Robiya <77631787+Robiyaxon@users.noreply.github.com>
2021-10-24 07:19:09 -0700

    a

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ import Aperator from './Component/Service/Aperator/Aperator';
 function App() {
   return (
     <div className="Servis_App">
-      <Route path={'/service/weddingRooms'} render={()=><Navbar />}/>
+      <Route path={'/service'} render={()=><Navbar />}/>
       <Route path="/service/weddingRooms" render={() => <Toyxona2 />} />
       <Route path="/service/weddingMachine" render={() => <CAR />} />
       <Route path="/service/artists" render={() => <Artist />} />
